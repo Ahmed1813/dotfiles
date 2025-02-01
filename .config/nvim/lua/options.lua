1812c42b0b3e43e3bd41e069467c06055dec2f7d
M. Ahmed <ahmedkk1813@gmail.com>
2025-02-01 22:30:31 +0500

    Set default relative linenumber and show dashboard when all buffers are closed.

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -2,5 +2,15 @@ require "nvchad.options"
 
 -- add yours here!
 
--- local o = vim.o
+local o = vim.o
+o.relativenumber = true
 -- o.cursorlineopt ='both' -- to enable cursorline!
+
+vim.api.nvim_create_autocmd("BufDelete", {
+  callback = function()
+    local bufs = vim.t.bufs
+    if #bufs == 1 and vim.api.nvim_buf_get_name(bufs[1]) == "" then
+      vim.cmd "Nvdash"
+    end
+  end,
+})
